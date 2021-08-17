46410c4f29e432f374f98b99021392214a3734a0
raquel <raquelanaroman@gmail.com>
2021-08-17 15:44:59 -0400

    commit tree 43b4c3a8474db48e21ca3fcc8abeb7627644f6fd

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :must_be_click_bate
+
+    CLICKBATE = [/Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i]
+
+    def must_be_click_bate
+        if CLICKBATE.none? { |b| b.match title}
+            errors.add(:title, "Title must contain clickbate nonsense")
+        end
+    end
+
 end
